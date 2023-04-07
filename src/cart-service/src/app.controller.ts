9c09adac2a0ab03c0135aff8d2921a5ddb9c84e2
ArtsiomRymarchuk <artsiom.rymarchuk@onepeloton.com>
2023-04-07 11:13:08 +0300

    use user service

--- src/cart-service/src/app.controller.ts
+++ src/cart-service/src/app.controller.ts
@@ -17,7 +17,7 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('api/auth/login')
   async login(@Request() req) {
-    const token = this.authService.login(req.user, 'basic');
+    const token = this.authService.login(req.body, 'basic');
 
     return  {
       statusCode: HttpStatus.OK,
